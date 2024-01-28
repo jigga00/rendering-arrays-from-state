2b66d742c772ee7fa678ccc80eed92a5d7ad5301
Tommie Durdin <74797799+jigga00@users.noreply.github.com>
2024-01-28 18:40:21 -0500

    Add files via upload

---
+++ Score.js
@@ -0,0 +1,11 @@
+// Score.js
+import React from "react";
+
+const Score = ({ score }) => {
+  return (
+    <div>
+      <p>Date: {score.date}</p>
+      <p>Score: {score.score}</p>
+    </div>
+  );
+};
